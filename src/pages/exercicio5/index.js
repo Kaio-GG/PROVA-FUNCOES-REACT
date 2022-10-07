594c31114757b6d75bc926cab38bade968aff6c5
kaio <kaiofdss@gmail.com>
2022-10-06 23:02:14 -0300

    sem dar conflito

---
+++ src/pages/exercicio5/index.js
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+import './index.scss'
+
+export default function Index() {
+    const [capac ,setcapac]=useState(0)
+    const [consumo, setconsumo] = useState(0)
+    const [dist, setdist] = useState(0)
+    const [resp , setresp]= useState(0)
+
+    function paradas (capacidade , consumo , distancia){
+        Number(capacidade,consumo,distancia)
+        let a = (capacidade*consumo)/700
+        Math.ceil(a)
+        setresp(a)
+    }
+
+    return(
+
+        <main className='mae'>
+        <div>
+            <h1>EXERCÍCIO 5</h1>
+        </div>
+        <div>
+            <input type='number' placeholder=' digite a capacidade' onChange={e => setcapac(e.target.value)}/>&ensp;
+            <input type='number' placeholder=' digite o consumo' value={diav} onChange={e => setconsumo(e.target.value)}/> &ensp;
+            <input type='number' placeholder=' digite a distancia' onChange={e => setdist(e.target.value)}/>&ensp;
+            <button onClick={() => paradas(capac,consumo , dist)}>CALCULAR</button>
+        </div>
+        {resp}
+    </main>
+    )
+
+
+
+
+
+}
